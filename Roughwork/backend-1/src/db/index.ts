f486ea1ffe8ce6b078df47c571f4b91ccacaf777
satyasootar <satya.sootar06@gmail.com>
2026-04-08 20:13:56 +0530

    Refactor code structure for improved readability and maintainability

--- Roughwork/backend-1/src/db/index.ts
+++ Roughwork/backend-1/src/db/index.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 
-export const db = drizzle(process.env.DATABASE_URL!);
+
+export const db = drizzle("postgresql://postgres:postgres@localhost:5432/chaicode");
--- Roughwork/backend-1/src/index.ts
+++ Roughwork/backend-1/src/index.ts
@@ -1,11 +1,13 @@
 import http from "http"
 import createServerApplication from "./app/index.js";
 
+
 function main(){
     const server = http.createServer(createServerApplication());
     const port = process.env.PORT || 3000
 
     server.listen(port, ()=>{
+        console.log("process.env.DATABASE_URL: ", process.env.DATABASE_URL);
         console.log(`The server is running at port: ${port}`);
     })
 }
